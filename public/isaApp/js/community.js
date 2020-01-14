ca26b6f3d2fd04d58d02402a722fad6b52e6b212
Rob Andrews <rob@aphlor.org>
2020-01-14 01:33:11 +0000

    consolidate autosuggest population and bootstrap

--- public/isaApp/js/community.js
+++ public/isaApp/js/community.js
@@ -1,8 +1,31 @@
 angular.module('infosecaware', [])
   .controller('communityController', ['$scope', '$http', function($scope, $http) {
-    $scope.master = {}
+    $scope.noIssues = []
+
+    $scope.fill = function(data) {
+      $scope.issues = angular.copy(data)
+    }
+
+    $scope.update = function() {
+      $http.get('/api/v1/getBoardData')
+        .then(
+          function(res) {
+            // success
+            $scope.fill(res.data.posts)
+          },
+          function() {
+            // error
+            console.log('failed to retrieve posts from server')
+          }
+        )
+    }
+
+    $scope.fill($scope.noIssues)
+    $scope.update()
   }])
 
-angular.element(function() {
-  angular.bootstrap(document, ['infosecaware'])
-})
+// load the tags and authors for filtering
+setupAuthorTagSuggestions(
+  document.getElementById('postTags'),
+  document.getElementById('postAudience')
+)
