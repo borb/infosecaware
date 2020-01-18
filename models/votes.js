cf480f15cf40159cdbb92c0c3b54e8efca3fdd5c
Rob Andrews <rob@aphlor.org>
2020-01-18 16:25:34 +0000

    add a votes collection

---
+++ models/votes.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+const ObjectId = mongoose.Types.ObjectId
+
+const votesSchema = new mongoose.Schema({
+  issueId: {
+    type: ObjectId,
+    required: true,
+    index: true
+  },
+  voteCaster: {
+    type: String,
+    required: true,
+    index: true
+  },
+  up: {
+    type: Boolean,
+    required: true,
+    default: true
+  },
+  castDate: {
+    type: Date,
+    required: true,
+    default: Date.now
+  }
+})
+
+mongoose.model('votes', votesSchema)
